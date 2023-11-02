c0a8da8f6fdb276707804171b067116037e74e4e
ziyiguo <123039540+VientoGuo@users.noreply.github.com>
2023-11-01 22:53:18 -0400

    Create EncodeStructure.cpp

---
+++ project/Server/EncodeStructure.cpp
@@ -0,0 +1,45 @@
+main(input_packet){
+CDC(input_packet;Boundaries_Array[]){
+// calculatw hash value of windows, output the position of doundaries
+// input: input_packet
+// output: Boundaries_Array: contains the position of doundaries
+}
+
+ChunkMatching(input_packet;Boundaries_Array[];UniqueChunks_Boundries[];DuplicatedChunks_Info[]){
+// detect duplicated chunks, output unique chunks boundaries and duplicated chunks info
+// input 1: input_packet
+// input 2: Boundaries_Array[]
+// output 1: UniqueChunks_Boundries[] indicates the unique chunks' boundries
+// output 2: DuplicatedChunks_Info[] indicates the duplicated chunks' information, 
+//         including its boundaries & indicate the position of the same unique chunk.
+
+  SHA(input_packet;Boundries_Array[];Hash_Array[]){
+  // base on positions of boundaries, calculate hash value of each chunk
+  // input 1: input_packet
+  // input 2: Boundaries_Array[]
+  // output: Hash_Array[]: indicates the hash value of each chunk 
+  }
+  ChunkDuplicationDetect(Boundaries_Array[];Hash_Array[];UniqueChunks_Boundaries[];DuplicatedChunks_Info[]){
+  //check duplication in Hash_Array,recor duplicated chunks info and unique chunks boundaries
+  // output: UniqueChunks_Boundaries[] : indicates the boundaries of unique chunks
+  // output: DuplicatedChunks:Info[] : indicates the duplicated chunks' information, 
+  //         including its boundaries & indicate the position of the same unique chunk.
+
+  }
+}
+LZW(input_packet;UniqueChunks_Boundaries[];Deduplicated_uniquechunks[]){
+// output deduplicated unique chunks
+// input1: input_packet
+// input2: UniqueChunks_Boundaries[]:indicates the boundaries of unique chunks
+// output: Deduplicated_uniquechunks[]: Deduplicated version of uniquechunks
+}
+Output_Function(UniqueChunks_Boundaries[];Deduplicated_uniquechunks[];DuplicatedChunks_Info[];output_packet){
+  // base on deduplicated unique chunks, and duplicated chunks' info generate output_packet
+  // input 1: UniqueChunks_Boundaries[]:indicates the boundaries of unique chunks
+  // input 2: Deduplicated_uniquechunks[]: Deduplicated version of uniquechunks
+  // input 3: DuplicatedChunks:Info[] : indicates the duplicated chunks' information, 
+  //         including its boundaries & indicate the position of the same unique chunk.
+  // output: output_packet: deduplicated version of input_packet
+}
+
+}
